2520b5d69645e7c1f9c1ee6289b7cfcc9a04af7b
RemcoDrolenga <rdrolenga@gmail.com>
2022-02-17 10:51:25 +0100

    Smaken afgerond

--- Papi-Gelato.py
+++ Papi-Gelato.py
@@ -3,18 +3,13 @@ def SorryDatSnapIkNiet():
 
 print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
-Aardbei = 0
-Chocolade = 0
-Munt = 0
-Vanille = 0
-
 def Stap1():
     HoeveelBolletjes = int(input("Hoeveel bolletjes wilt u? "))
     if HoeveelBolletjes >= 1 and HoeveelBolletjes <=3:
-        return HoeveelBolletjes
+        Stap2(HoeveelBolletjes)
     elif HoeveelBolletjes >= 4 and HoeveelBolletjes <= 8:
         print("Dan krijgt u van mij een bakje met",HoeveelBolletjes,"bolletjes.")
-        return HoeveelBolletjes
+        Stap2(HoeveelBolletjes)
     elif HoeveelBolletjes >= 8:
         print("Sorry, zulke grote bakken hebben we niet.")
         Stap1()
@@ -22,23 +17,27 @@ def Stap1():
         SorryDatSnapIkNiet()
         Stap1()
 
-def Stap4(HoeveelBolletjes):
+def Stap2(HoeveelBolletjes):
     Aardbei = 0
     Chocolade = 0
     Munt = 0
     Vanille = 0
-    for i in range(1,HoeveelBolletjes):
-        Smaken = input("Welke smaak wilt u voor bolletje nummer "+str(i)+" ? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?")
+    for i in range(1,HoeveelBolletjes+1):
+        Smaken = input("Welke smaak wilt u voor bolletje nummer "+str(i)+" ? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ").upper()
         if Smaken == "A":
             Aardbei += 1
         elif Smaken == "C":
             Chocolade += 1
-        elif Munt == "M":
+        elif Smaken == "M":
             Munt += 1
         elif Smaken == "V":
             Vanille += 1
+        else:
+            SorryDatSnapIkNiet()
+            Stap2(HoeveelBolletjes)
+    Stap3(HoeveelBolletjes)
 
-def Stap2(HoeveelBolletjes):
+def Stap3(HoeveelBolletjes):
     if HoeveelBolletjes <= 3:
         BakjeOfHoorntje = input("Wilt u deze "+str(HoeveelBolletjes)+" bolletje(s) in A) een hoorntje of B) een bakje? ").upper()
         if BakjeOfHoorntje == "A" or BakjeOfHoorntje == "B":
@@ -46,27 +45,22 @@ def Stap2(HoeveelBolletjes):
                BakjeOfHoorntje = "hoorntje"
             else:
                 BakjeOfHoorntje = "bakje"
-            return BakjeOfHoorntje
+            Stap4(BakjeOfHoorntje,HoeveelBolletjes)
         else:
             SorryDatSnapIkNiet()
-            Stap2()
+            Stap3()
     else:
         BakjeOfHoorntje = "bakje"
-        return BakjeOfHoorntje
+        Stap4(BakjeOfHoorntje,HoeveelBolletjes)
 
-def Stap3(BakjeOfHoorntje,HoeveelBolletjes):
+def Stap4(BakjeOfHoorntje,HoeveelBolletjes):
     NogMeer = input("Hier is uw "+BakjeOfHoorntje+" met "+str(HoeveelBolletjes)+" bolletje(s). Wilt u nog meer bestellen? (Y/N) ")
     if NogMeer == "Y" or NogMeer == "y":
-        AanTalBolIs = Stap1()
-        BakjeOfHoorntje = Stap2(AanTalBolIs)
-        Stap3(BakjeOfHoorntje,AanTalBolIs)
+        Stap1()
     elif NogMeer == "N" or NogMeer == "n":
         print("Bedankt en tot ziens!")
     else:
         SorryDatSnapIkNiet()
-        Stap3()
+        Stap4()
 
-AanTalBolIs = Stap1()
-AanTalBolIs = Stap4(AanTalBolIs)
-BakjeOfHoorntje = Stap2(AanTalBolIs)
-Stap3(BakjeOfHoorntje,AanTalBolIs)
+Stap1()
